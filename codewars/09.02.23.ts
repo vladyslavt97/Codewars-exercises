76e98066fd4dfefa2223a2b8d18820c0a4612f02
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-02-09 15:54:00 +0100

    latest challenges

--- codewars/09.02.23.ts
+++ codewars/09.02.23.ts
@@ -7,3 +7,20 @@ export function powersOfTwo(n:number):number[]{
 }
 
 // console.log(powersOfTwo(5))
+
+
+export function humanYearsCatYearsDogYears(humanYears: number): [number, number, number] | undefined {
+  // Your code here!
+  if (humanYears === 1){
+    return [humanYears, 15, 15];
+  } else if (humanYears === 2){
+    return [humanYears, 24, 24];
+  } else if(humanYears > 2){
+    let catYears = (humanYears-2)*4 + 24;
+    let dogYears = (humanYears-2)*5 + 24;
+    return [humanYears, catYears, dogYears];
+  }
+  return undefined;
+}
+
+console.log(humanYearsCatYearsDogYears(25));
