b95a14a6603adff7493119dadf0502271a33dc1f
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-02-23 16:06:28 +0100

    Katas:)

---
+++ 23.02.23.js
@@ -0,0 +1,32 @@
+//Replace With Alphabet Position
+function alphabetPosition(text) {
+  let numbers = [];
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  let arr = alphabet.split('')
+  for (let i = 0; i < text.length; i++){
+    for (let a = 0; a < arr.length; a++){
+      let letter = text[i].toLowerCase()
+      if(letter === alphabet[a]){
+        numbers.push(alphabet.indexOf(letter) + 1)
+      }
+    }
+  }
+  let stringComas = numbers.toString().replaceAll(',', ' ')
+  return stringComas;
+}
+
+
+
+//Moving Zeros To The End
+function moveZeros(arr) {
+  let newArr = [];
+  let zeros = [];
+  for (let i = 0; i < arr.length; i++){
+    if ( arr[i] === 0){
+      zeros.push(arr[i]);
+    } else {
+      newArr.push(arr[i])
+    }
+  }
+  return newArr.concat(zeros)
+}
