98cce41aad9ec9c5ed1d68ee17c7fd8e8a882ca3
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-02-23 16:52:38 +0100

    latest change

--- 23.02.23.js
+++ 23.02.23.js
@@ -30,3 +30,12 @@ function moveZeros(arr) {
   }
   return newArr.concat(zeros)
 }
+
+//Power
+function numberToPower(number, power){
+  let total = 1;
+  for (let i = 1; i <= power; i++){
+    total = total * number;
+  }
+  return total;
+}
