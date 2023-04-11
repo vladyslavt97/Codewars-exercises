035b8ca07d03bcfaa733e6c85ee87ada62881b9b
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-04-11 16:52:43 +0200

    Tasks for Espressions Statements

--- codewars/April/11.04.23.java
+++ codewars/April/11.04.23.java
@@ -16,3 +16,49 @@ public class AreaCalculator {
         }
     }
 }
+
+// Minutes To Years and Days Calculator
+public class MinutesToYearsDaysCalculator {
+    // write your code here
+    public static void printYearsAndDays(long minutes) {
+        if (minutes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            long years = minutes / (60 * 24 * 365);
+            long remainingMinutes = minutes % (60 * 24 * 365);
+            long days = remainingMinutes / (60 * 24);
+            System.out.println(minutes + " min = " + years + " y and " + days + " d");
+        }
+    }
+}
+
+// Equality Printer
+public class IntEqualityPrinter {
+    // write your code here
+    public static void printEqual(int a, int b, int c) {
+        if (a < 0 || b < 0 || c < 0) {
+            System.out.println("Invalid Value");
+        } else if (a == b && b == c) {
+            System.out.println("All numbers are equal");
+        } else if (a != b && b != c && a != c) {
+            System.out.println("All numbers are different");
+        } else {
+            System.out.println("Neither all are equal or different");
+        }
+    }
+}
+
+
+// Playing Cat
+public class PlayingCat {
+    // write your code here
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        if (summer && (temperature <= 45 && temperature >= 25)) {
+            return true;
+        } else if (!summer && (temperature <= 35 && temperature >= 25)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
