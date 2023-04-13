d7f01b0f2c052e69e20cf016a3a0bf28eafe6567
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-04-13 17:34:17 +0200

    Last Digit Checker

--- codewars/April/13.04.23.java
+++ codewars/April/13.04.23.java
@@ -14,3 +14,85 @@ public class FirstLastDigitSum {
         }
     }
 }
+
+// Even Digit Sum
+public class EvenDigitSum {
+    // write your code here
+    public static int getEvenDigitSum(int number) {
+        if (number >= 0) {
+            int sumof = 0;
+            String numberString = Integer.toString(number); // Convert number to string
+            for (int i = 0; i < numberString.length(); i++) {
+                char digitChar = numberString.charAt(i); // Get the character at the current position
+                int digit = Character.getNumericValue(digitChar);
+                if (digit % 2 == 0) {
+                    sumof += digit;
+                }
+            }
+            return sumof;
+        } else {
+            return -1;
+        }
+    }
+}
+
+// Shared Digit
+public class SharedDigit {
+    // write your code here
+    public static boolean hasSharedDigit(int a, int b) {
+        if ((a >= 10 && a <= 99) && (b >= 10 && b <= 99)) {
+            int valueA = 0;
+            String aStr = Integer.toString(a);
+            for (int i = 0; i <= aStr.length(); i++) {
+                char digitChar = aStr.charAt(i); // Get the character at the current position
+                int digit = Character.getNumericValue(digitChar);
+                if (digit == 2) {
+                    valueA++;
+                }
+            }
+            int valueB = 0;
+            String bStr = Integer.toString(b);
+            for (int i = 0; i <= bStr.length(); i++) {
+                char digitChar = bStr.charAt(i); // Get the character at the current position
+                int digit = Character.getNumericValue(digitChar);
+                if (digit == 2) {
+                    valueB++;
+                }
+            }
+            if (valueB == valueA) {
+                return true;
+            }
+            return false;
+        } else {
+            return false;
+        }
+    }
+}
+
+// Last Digit Checker
+public class LastDigitChecker {
+    // write your code here
+    public static boolean hasSameLastDigit(int a, int b, int c) {
+        if ((a >= 10 && a <= 1000) && (b >= 10 && b <= 1000) && (c >= 10 && c <= 1000)) {
+            System.out.println(a % 10);
+            int lastA = a % 10;
+            int lastB = b % 10;
+            int lastC = c % 10;
+            if ((lastA == lastB) || (lastB == lastC) || (lastC == lastA)) {
+                return true;
+            }
+            return false;
+
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isValid(int d) {
+        if (d >= 10 && d <= 1000) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
