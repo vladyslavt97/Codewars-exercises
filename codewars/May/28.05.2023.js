84dfaff16b6a67ca9396dee56433a56f12b2d0e6
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-05-28 22:04:17 +0200

    last one

--- codewars/May/28.05.2023.js
+++ codewars/May/28.05.2023.js
@@ -1,3 +1,3 @@
 //Training JS #18: Methods of String object--concat() split() and its good friend join()
-
+//to do it more often
 const splitAndMerge = (string, separator) => string.split(" ").map(el => el.split("").join(separator)).join(" ");
