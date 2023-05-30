9f1da6a4a96615485b152eb6c96485642e759495
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-05-30 21:06:26 +0200

    one more

--- codewars/May/28.05.2023.js
+++ codewars/May/28.05.2023.js
@@ -1,3 +1,18 @@
 //Training JS #18: Methods of String object--concat() split() and its good friend join()
 //to do it more often
 const splitAndMerge = (string, separator) => string.split(" ").map(el => el.split("").join(separator)).join(" ");
+
+
+function points(games) {
+  let xCount = 0;
+  for (let i = 0; i < games.length; i++){
+    if(parseInt(games[i]) === parseInt(games[i].slice(-1))){
+      xCount++;
+    } else if (parseInt(games[i]) > games[i].slice(-1)){
+      xCount += 3;
+    } else {
+      continue;
+   }
+  }
+  return xCount;
+}
